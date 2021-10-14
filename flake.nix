f1ccc4e668bad74907f641a4ac6427bf466cd627
Mate Hajzso <ajz@lambd.ax>
2021-10-14 20:17:19 +0200

    fix merge screw-up

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     c4.url = "github:fossar/composition-c4";
-
-    # NOTE: change this to self if flake gets merged upstream
     movim-src.url = "github:movim/movim";
     movim-src.flake = false;
   };
@@ -26,24 +24,16 @@
               php = (
                 pkgs.php74.withExtensions (
                   { enabled, all }:
-                    with all; [ xml curl mbstring imagick gd pgsql ]
+                    with all; enabled ++ [ curl mbstring imagick gd pgsql xml ]
                 )
               );
-              defaultConfigFile = "";
             in
               rec {
-                defaultPackage = packages.movim;
-
-                packages.movim =
-                  #packages.movim = pkgs.callPackage (
-                  #{ configFile ? defaultConfigFile }:
-                  #let
-                  #  configFile' = pkgs.writeText "db.inc.php" "${configFile}";
-                  #in
-                  pkgs.stdenv.mkDerivation rec {
+                #defaultPackage = packages.${system}.movim;
+                packages = {
+                  movim = pkgs.stdenv.mkDerivation {
                     pname = "movim";
-                    # Generate a user-friendly version numer
-                    version = "${builtins.substring 0 8 self.lastModifiedDate}-${self.shortRev or "dirty"}";
+                    version = "0.1.1";
                     src = movim-src;
 
                     composerDeps = pkgs.c4.fetchComposerDeps {
@@ -65,24 +55,20 @@
                       runHook postInstall
                     '';
                   };
-                #) {};
+                };
 
                 devShell = pkgs.mkShell {
                   nativeBuildInputs = [
-                    php
-                    pkgs.php74Packages.composer
+                    pkgs.php74.composer
+                    (
+                      pkgs.php74.withExtensions (
+                        { enabled, all }:
+                          with all; enabled ++ [ curl mbstring imagick gd pgsql xml ]
+                      )
+                    )
                   ];
                 };
 
-                #nixosModules.movim = { config, pkgs, lib, ... }: let
-                #  inherit (lib) mkOption mkEnableOption mkIf mkDefault types;
-                #  inherit (pkgs) writeShellScript writeText rsync postgresql;
-                #  cfg = config.services.movim;
-                #  package = packages.movim.override {
-                #    configFile = "";
-                #  };
-                #in
-                #  {};
 
               }
         );
