a35eeafa4a9d58e362d3e3ad9e6e31765590e857
Mate Hajzso <ajz@lambd.ax>
2021-10-14 20:23:31 +0200

    fix merge screw-up

--- flake.nix
+++ flake.nix
@@ -3,13 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     c4.url = "github:fossar/composition-c4";
-<<<<<<< HEAD
-=======
-
-    # NOTE: change this to self if flake gets merged upstream
     movim-src.url = "github:movim/movim";
-    movim-src.flake = false;
->>>>>>> flake_only
   };
 
   outputs =
@@ -17,10 +11,7 @@
     , nixpkgs
     , flake-utils
     , c4
-<<<<<<< HEAD
-=======
     , movim-src
->>>>>>> flake_only
     }:
       let
         overlays = [ c4.overlay ];
@@ -32,7 +23,6 @@
               php = (
                 pkgs.php74.withExtensions (
                   { enabled, all }:
-<<<<<<< HEAD
                     with all; enabled ++ [ curl mbstring imagick gd pgsql xml ]
                 )
               );
@@ -43,36 +33,11 @@
                   movim = pkgs.stdenv.mkDerivation {
                     pname = "movim";
                     version = "0.1.1";
-                    src = ./.;
-
-                    composerDeps = pkgs.c4.fetchComposerDeps {
-                      #inherit src;
-                      src = ./.;
-=======
-                    with all; [ xml curl mbstring imagick gd pgsql ]
-                )
-              );
-              defaultConfigFile = "";
-            in
-              rec {
-                defaultPackage = packages.movim;
-
-                packages.movim =
-                  #packages.movim = pkgs.callPackage (
-                  #{ configFile ? defaultConfigFile }:
-                  #let
-                  #  configFile' = pkgs.writeText "db.inc.php" "${configFile}";
-                  #in
-                  pkgs.stdenv.mkDerivation rec {
-                    pname = "movim";
-                    # Generate a user-friendly version numer
-                    version = "${builtins.substring 0 8 self.lastModifiedDate}-${self.shortRev or "dirty"}";
                     src = movim-src;
 
                     composerDeps = pkgs.c4.fetchComposerDeps {
                       #inherit src;
                       src = movim-src;
->>>>>>> flake_only
                     };
 
                     nativeBuildInputs = [
@@ -89,7 +54,6 @@
                       runHook postInstall
                     '';
                   };
-<<<<<<< HEAD
                 };
 
                 devShell = pkgs.mkShell {
@@ -104,26 +68,6 @@
                   ];
                 };
 
-=======
-                #) {};
-
-                devShell = pkgs.mkShell {
-                  nativeBuildInputs = [
-                    php
-                    pkgs.php74Packages.composer
-                  ];
-                };
-
-                #nixosModules.movim = { config, pkgs, lib, ... }: let
-                #  inherit (lib) mkOption mkEnableOption mkIf mkDefault types;
-                #  inherit (pkgs) writeShellScript writeText rsync postgresql;
-                #  cfg = config.services.movim;
-                #  package = packages.movim.override {
-                #    configFile = "";
-                #  };
-                #in
-                #  {};
->>>>>>> flake_only
 
               }
         );
